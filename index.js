b40362b7c8086938eef40b24286ff8184ac80e6a
bruno montebello <brunormontebello@hotmail.com>
2020-10-14 23:38:56 +0100

    Grab html itens

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var tarefas = [
+  'Fazer cafe',
+  'estudar JS',
+  'Acessar comunidade'
+];
+
+function renderTarefas() {
+  for(tarefa of tarefas) {
+    var tarefaElement = document.createElement('li');
+    var tarefaText = document.createTextNode(tarefa);
+
+    tarefaElement.appendChild(tarefaText);
+    listElement.appendChild(tarefaElement);
+  }
+}
+
+renderTarefas();
+
+function addTarefa() {
+  var tarefaText = inputElement.value;
+
+  tarefas.push(tarefaText);
+  inputElement.value = ''; //makes the input box blank again
+  renderTarefas();
+}
+
+buttonElement.onclick = addTarefa; // chama a função addTarefa qnd o botao é clicado
