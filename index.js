d7a73eed8242eec96861358b01d4f5b5b82cbaa3
bruno montebello <brunormontebello@hotmail.com>
2020-10-15 09:51:57 +0100

    Cria o link p excluir a tarefa

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ var tarefas = [
 ];
 
 function renderTarefas() {
+
+  //.innerHTML pega todo o html dentro do elemento.
+  //essa linha exclui a lista de tarefas ja existente ao add uma nova. assim nao há repetição
+  listElement.innerHTML = '';
   for(tarefa of tarefas) {
     var tarefaElement = document.createElement('li');
     var tarefaText = document.createTextNode(tarefa);
 
+    //cria o link que exclui a tarefa
+    var linkElement = document.createElement('a'); //cria o elemento HTML <a> e guarda na variavel
+    var linkText = document.createTextNode('Excluir'); //cria e armazena o texto que quero no elemento <a>
+    linkElement.appendChild(linkText); //embute o texto dentro do elemento
+    linkElement.setAttribute('href', '#'); //cria atributo href p q <a> seja um link
+
     tarefaElement.appendChild(tarefaText);
+    tarefaElement.appendChild(linkElement); //add o botao de excluir a cada tarefa que aparece
+
     listElement.appendChild(tarefaElement);
   }
 }
